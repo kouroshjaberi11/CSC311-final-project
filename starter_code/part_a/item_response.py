Fixed some things in item response, added validation cost calculation in NN for part d

starter_code/part_a/item_response.py
@@ -31,17 +31,10 @@ def neg_log_likelihood(data, theta, beta):
     #####################################################################
     log_lklihood = 0.
 
-    # for i in range(len(data['user_id'])):
-    #     if data['is_correct'] == 1:
-    #         log_lklihood += np.log(sigmoid(theta[data['user_id'][i]] - beta[data['question_id'][i]]))
-    #     else:
-    #         # print(theta[data['user_id'][i])
-    #         log_lklihood += np.log(1 - sigmoid(theta[data['user_id'][i]] - beta[data['question_id'][i]]))
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             if not np.isnan(data[i, j]):
-                # print(data[i, j])
-                log_lklihood += data[i, j] * ((theta[i] - beta[j]) - np.log(1 + np.exp(theta[i] - beta[j])))
+                log_lklihood += (data[i, j] * (theta[i] - beta[j])) - np.log(1 + np.exp(theta[i] - beta[j]))
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -110,7 +103,7 @@ def irt(data, val_data, lr, iterations):
 
     for i in range(iterations):
         neg_lld = neg_log_likelihood(data, theta=theta, beta=beta)
-        _, score = evaluate(data=val_data, theta=theta, beta=beta)
+        score = evaluate(data=val_data, theta=theta, beta=beta)
         val_acc_lst.append(score)
         print("NLLK: {} \t Score: {}".format(neg_lld, score))
         theta, beta = update_theta_beta(data, lr, theta, beta)
@@ -134,7 +127,7 @@ def evaluate(data, theta, beta):
         x = (theta[u] - beta[q])
         p_a = sigmoid(x)
         pred.append(p_a >= 0.5)
-    return pred, np.sum((data["is_correct"] == np.array(pred))) \
+    return np.sum((data["is_correct"] == np.array(pred))) \
            / len(data["is_correct"])
 
 
@@ -144,18 +137,15 @@ def main():
     sparse_matrix = load_train_sparse("../data").toarray()
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
-    print(sparse_matrix[1, 1])
     #####################################################################
     # TODO:                                                             #
     # Tune learning rate and number of iterations. With the implemented #
     # code, report the validation and test accuracy.                    #
     #####################################################################
-    # print(train_data.shape)
-    iteration = 100  # needs tuning
+    iteration = 69
     learn = 0.005
     theta, beta, acc = irt (sparse_matrix, val_data, learn, iteration)
-    pred, test_acc = evaluate (test_data, theta, beta)
-    print(pred)
+    test_acc = evaluate (test_data, theta, beta)
     print("Final Validation accuracy: " + str(acc))
     print("Final Test Accuracy: " + str(test_acc))
     #####################################################################
@@ -167,7 +157,6 @@ def main():
     # Implement part (d)                                                #
     #####################################################################
     j_s = train_data.get('question_id')[0:3]
-    print(j_s)
 
     p_j = np.zeros((3, len(theta)))
 